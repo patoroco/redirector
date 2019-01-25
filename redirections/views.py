08dc5e4879b365e4f98800484eee2593b2b466d6
Jorge Maroto <patoroco@gmail.com>
2019-01-25 21:27:15 +0100

    Catch full path

--- redirections/views.py
+++ redirections/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("HELLO")
+    return HttpResponse(f"Índice")
+
+
+def redirect(request, path):
+    return HttpResponse(f"Redirect => {request.META['HTTP_HOST']} - {path}")
