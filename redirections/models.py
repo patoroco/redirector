d40f280ffde2f8568b5a9728c18bc240529f1f66
Jorge Maroto <patoroco@gmail.com>
2019-01-25 23:36:06 +0100

    Adding host
    Comment

--- redirections/models.py
+++ redirections/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Redirection(models.Model):
+    host = models.CharField(max_length=100)
     path = models.CharField(max_length=200, blank=True)
     redirection = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published', auto_now_add=True)
+
+    # Únicos
+    class Meta:
+        unique_together = ('host', 'path')
