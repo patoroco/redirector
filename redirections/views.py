b9de7f9764272dbe7e1bb5c28582a78a78adcf98
Jorge Maroto <patoroco@gmail.com>
2019-01-26 00:32:58 +0100

    Clean up

--- redirections/views.py
+++ redirections/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.http import HttpResponsePermanentRedirect
 from .models import Redirection
-import re
-
-
-def loader_io(request):
-    return HttpResponse('loaderio-700a3036a753553143eacb48bbbf9684')
-
-
-def index(request):
-    return HttpResponse(f"Índice")
 
 
 def redirect(request, path):
-    # regexp = re.compile('.*TelegramBot.*')
-    # if regexp.search(request.META['HTTP_USER_AGENT']):
-    #     return HttpResponse('<title>Ocultando a Telegram</title>')
-
     redirection = get_object_or_404(
         Redirection,
         host=request.META['HTTP_HOST'],
