399f0fcddb0c61cdd632d64c8955b6cd0f1619d8
Jorge Maroto <patoroco@gmail.com>
2019-03-29 21:45:53 +0100

    Typo

--- redirections/factories.py
+++ redirections/factories.py
@@ -6,7 +6,7 @@ class RedirectionFactory(object):
     @staticmethod
     def get_with_fallback(host, path):
         """
-        Look for a pair (host, path), and if it's not found, it will return `default` value.
+        Looks for a pair (host, path), and if it's not found, it will return `default` value.
         If at the end it doesn't have a `default` value, a DoesNotExist exception will be raised.
         """
         try:
