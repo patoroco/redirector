d4536bf7373e17cb69a8f800055b0756c50feb91
Jorge Maroto <patoroco@gmail.com>
2023-07-16 01:16:25 +0200

    preparing

---
+++ djredirect/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *  # noqa
+
+STORAGE_DIR = "/app/storage/"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(STORAGE_DIR, "db.sqlite3"),
+    }
+}
+
+STATIC_ROOT = os.path.join(STORAGE_DIR, "staticfiles")
+
+STATIC_URL = '/static/'
